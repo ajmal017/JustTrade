762e907675f1918033f34bd42485b6c2895789eb
wusimo <szw184@psu.edu>
2016-11-04 14:14:29 -0400

    first draft models

--- JustTrade/JustTrade/task/models.py
+++ JustTrade/JustTrade/task/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from symbols.model import symbols
+from strategy.model import strategies
+from django.utils import timezone
 # Create your models here.
+class tradingTask(models.Model):
+	is_active = models.BooleanField(default=True)
+	waitingtime = models.IntegerField(blank=False, null=False)  # number of seconds.
+	symbol = models.ForeignKey(symbols,blank = False)
+	strategy = models.ForeignKey(strategies,blank = False)
+
+
+	trade_config_json = models.TextField(blank=True, null=True)
+
+
+class tradeLog(models.Model):
+	trade_type = models.ForeignKey(tradingTask,blank = False)
+	alert_time = models.DateTimeField(default=timezone.now, null=True)
